2c98cb5b35fc1382d50efcd7f6fa08fe019b1499
KILABID <aldimurad@gmail.com>
2023-08-13 10:51:27 +0700

    fix back button

--- app/src/main/java/com/latihanpbo/topupgenius/Home/CaraPembelian.java
+++ app/src/main/java/com/latihanpbo/topupgenius/Home/CaraPembelian.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.latihanpbo.topupgenius.R;
 
@@ -15,7 +16,7 @@ public class CaraPembelian extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cara_pembelian);
 
-        Button backBtn = findViewById(R.id.btnBack);
+        ImageView backBtn = findViewById(R.id.btnBack);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
